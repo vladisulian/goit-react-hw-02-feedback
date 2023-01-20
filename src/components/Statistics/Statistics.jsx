368e5a33718c62c334bd8f7b3ca259104675d8fe
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-01-20 22:21:58 +0200

    edit styes

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,13 +3,16 @@ export const Statistics = ({ good, neutral, bad, total, positivePercents }) => {
   return (
     <div className="statistics">
       <h1>Statistics</h1>
-      {/* good, neutra, bad feedback's */}
-      <p>Good: {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      {/* total, positive percents */}
-      <p>Total: {total}</p>
-      <p>Positive feedback: {positivePercents}</p>
+
+      <div className="statistics__data">
+        {/* good, neutra, bad feedback's */}
+        <p>Good: {good}</p>
+        <p>Neutral {neutral}</p>
+        <p>Bad {bad}</p>
+        {/* total, positive percents */}
+        <p>Total: {total}</p>
+        <p>Positive feedback: {positivePercents}</p>
+      </div>
     </div>
   );
 };
